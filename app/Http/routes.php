4bb916a8b538f0ec5ee7c8857a8095ef9b47e391
develop <1802716036@qq.com>
2017-09-22 11:48:03 +0800

    2017.9.22 框架

---
+++ app/Http/routes.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+use App\Entity\Member;
+
+Route::get('/', function () {
+    return view('login');
+});
+
+Route::get('test',function (){
+    $m = Member::all();
+
+    return $m;
+});
---
+++ resources/views/login.blade.php
@@ -0,0 +1,16 @@
+@extends('master')
+
+@include('component.loading')
+
+@include('component.dialog')
+
+@section('title','登录')
+
+@section('content')
+{{--<p>登录</p>--}}
+@endsection
+
+@section('my-js')
+
+
+@endsection
